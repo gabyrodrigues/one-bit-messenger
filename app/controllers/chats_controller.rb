f9c3702ea3fd5bd23fee32742788bbb51bfbf7d0
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-04-26 19:03:57 -0300

    listando chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,14 @@
 class ChatsController < ApplicationController
-    def show
-        @user = User.find(params[:id])
-        #exibir mensagem trocadas
-        @messages = Message.where(sender: current_user, receiver: @user).or(Message.where(sender: @user, receiver: current_user)).order(:created_at)
+		def index 
+			#listar todos os usuários com quem já houve troca de mensagens
+			@users = User.joins(:sent).where(messages: { receiver_id: current_user }) #mensagens que eu recebi
+			@users += User.joins(:received).where(messages: { sender_id: current_user }) #lista usuários: mensagens que eu enviei
+			@users.uniq!
+		end
+
+		def show
+			@user = User.find(params[:id])
+			#exibir mensagem trocadas
+			@messages = Message.where(sender: current_user, receiver: @user).or(Message.where(sender: @user, receiver: current_user)).order(:created_at)
     end
 end
