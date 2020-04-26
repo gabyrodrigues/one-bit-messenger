c38b9031a9eed5b0c56fbf1c2d3667d625a8bc9e
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-04-26 18:21:00 -0300

    enviando e recebendo mensagens de um contato

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,7 @@
+class ChatsController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+        #exibir mensagem trocadas
+        @messages = Message.where(sender: current_user, receiver: @user).or(Message.where(sender: @user, receiver: current_user)).order(:created_at)
+    end
+end
